d12b931eb600b6e4a84c01481e4512149cfa82be
Aralcom <salah@aral.co>
2017-09-17 12:55:53 +0300

    Update

--- l10n_iq/__manifest__.py
+++ l10n_iq/__manifest__.py
@@ -7,19 +7,21 @@
     'author': 'www.Aral.co',
     'category': 'Localization',
     'description': """
-Odoo localization for most  Republic of Iraq.
-
-This initially includes chart of accounts of Iraq.
-
-If you need the payroll rules for Iraq please contact sales@aral.co .
-""",
+        
+        Republic of Iraq Standard Accounting System by Aral
+        This includes chart of accounts of Iraq. based of the standards
+        If you need the payroll rules for Iraq please contact sales@aral.co .
+        """,
     'website': 'http://www.aral.co',
     'depends': ['account', 'l10n_multilang'],
     'data': [
-        'data/account_chart_template_data.xml',
-        'data/account.account.template.csv',
-        'data/l10n_iq_chart_data.xml',
-        'data/account_chart_template_data.yml',
-    ],
+             'data/account_chart_template_data.xml',
+             'data/account.account.template.csv',
+             'data/l10n_iq_chart_data.xml',
+             'data/account_chart_template_data.yml',
+             ],
+             'price': '2049.99',
+             'currency': 'EUR',
+             
     'post_init_hook': 'load_translations',
 }
